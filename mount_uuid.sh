25a46aba724016affcabde1ebf77198e210bbf30
sowmiya r <88027669+sowmiya34@users.noreply.github.com>
2026-09-11 11:03:59 +0530

    Update mount_uuid.sh

--- mount_uuid.sh
+++ mount_uuid.sh
@@ -9,20 +9,20 @@
 
 # Display filesystem UUID
 
-blkid
+
 
 
 # Create mount directory
 
-mkdir -p /mnt/uuiddata
+
 
 
 # Mount filesystem using UUID
 # Replace YOUR_UUID with actual UUID
 
-mount UUID=YOUR_UUID /mnt/uuiddata
+
 
 
 # Display mounted filesystem
 
-df -h
+
