4b8f73b0beca33933db48fc4a30be734361b2a27
APARNAMALATHI <174698753+APARNAMALATHI@users.noreply.github.com>
2026-09-11 11:32:39 +0530

    Update mount_uuid.sh

--- mount_uuid.sh
+++ mount_uuid.sh
@@ -2,27 +2,25 @@
 
 # ==========================================
 # Mount Filesystem Using UUID
-# Student Name:
-# Roll Number:
+# Student Name:aparna
+# Roll Number:1U24IT010
 # ==========================================
 
 
 # Display filesystem UUID
-
+sudo blkid
 
 
 
 # Create mount directory
+sudo mkdir -p /mnt/mydisk
 
 
 
 
 # Mount filesystem using UUID
 # Replace YOUR_UUID with actual UUID
-
-
-
+sudo mount UUID=YOUR_UUID /mnt/mydisk
 
 # Display mounted filesystem
-
-
+df -h /mnt/mydisk
